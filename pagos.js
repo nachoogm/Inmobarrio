04b7f9041e423f643749b645a3dfdec7aab88f15
nachoogm <113169650+nachoogm@users.noreply.github.com>
2025-10-15 21:27:33 +0200

    Delete pagos.js

--- pagos.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPagos } = require('../controllers/pagosController');
-
-// GET todos los pagos
-router.get('/', getPagos);
-
-module.exports = router;
